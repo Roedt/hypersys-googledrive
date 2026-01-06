2a3c06a9cdd473fe7dc866d0d42d0828b0170c38
Mads Opheim <mads.opheim@gmail.com>
2026-01-06 23:16:36 +0100

    Får deserialisering til å funke

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,17 +16,20 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
-    implementation("org.apache.camel.quarkus:camel-quarkus-google-drive")
-    implementation(enforcedPlatform("${quarkusPlatformGroupId}:quarkus-camel-bom:${quarkusPlatformVersion}"))
     implementation("io.quarkiverse.googlecloudservices:quarkus-google-cloud-secret-manager")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:quarkus-google-cloud-services-bom:${quarkusPlatformVersion}"))
-    implementation("io.quarkiverse.microprofile:quarkus-microprofile:3.4.1")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.quarkus:quarkus-resteasy-jackson")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
+
+    implementation("io.quarkus:quarkus-rest-client-jackson")
+    implementation("io.quarkus:quarkus-rest-jackson")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    implementation("io.quarkus:quarkus-smallrye-jwt")
+    implementation("io.quarkus:quarkus-smallrye-jwt-build")
 }
 
 group = "no.roedt"
@@ -57,6 +60,6 @@ kotlin {
         Vi bruker egentlig bare konstruktør-varianten, men vil egentlig helst holde oss til kotlin sin standardvariant
         Så når dette er blitt standarden i kotlin - som det skal bli - så kan vi skru av denne
          */
-        freeCompilerArgs = listOf("-Xannotation-default-target=param-property")
+        freeCompilerArgs = listOf("-Xannotation-default-target=param-property", "-Xjvm-default=all")
     }
 }
